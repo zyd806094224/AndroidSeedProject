312c59b70776b638e4c0c129e64266ed589f809d
zhaoyudong <zhaoyudong@58.com>
2024-09-09 12:48:24 +0800

    SmartRefreshLayout初始化相关，解决刷新bug

--- app/src/main/java/com/demo/androidseedproject/MyApplication.kt
+++ app/src/main/java/com/demo/androidseedproject/MyApplication.kt
@@ -3,6 +3,9 @@ package com.demo.androidseedproject
 import android.app.Application
 import com.alibaba.android.arouter.launcher.ARouter
 import com.demo.framework.helper.AppHelper
+import com.scwang.smart.refresh.footer.ClassicsFooter
+import com.scwang.smart.refresh.header.ClassicsHeader
+import com.scwang.smart.refresh.layout.SmartRefreshLayout
 
 /**
  * @Description:
@@ -16,5 +19,20 @@ class MyApplication : Application() {
         super.onCreate()
         AppHelper.init(this, BuildConfig.DEBUG)
         ARouter.init(AppHelper.getApplication())
+        initRefreshLayoutTask()
+    }
+
+    private fun initRefreshLayoutTask() {
+        //设置全局的Header构建器
+        SmartRefreshLayout.setDefaultRefreshHeaderCreator { context, layout ->
+            layout.setPrimaryColorsId(android.R.color.white)
+            //            CustomRefreshHeader(context)
+            ClassicsHeader(context)
+        }
+        //设置全局的Footer构建器
+        SmartRefreshLayout.setDefaultRefreshFooterCreator { context, layout ->
+            //指定为经典Footer，默认是 BallPulseFooter
+            ClassicsFooter(context)
+        }
     }
 }
--- mod_main/src/main/java/com/demo/main/ui/home/HomeFragment.kt
+++ mod_main/src/main/java/com/demo/main/ui/home/HomeFragment.kt
@@ -2,9 +2,6 @@ package com.demo.main.ui.home
 
 import android.graphics.Typeface
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
 import android.util.SparseArray
 import android.util.TypedValue
 import android.view.View
@@ -28,7 +25,7 @@ import com.scwang.smart.refresh.layout.listener.OnRefreshListener
  * @author:  zhaoyudong
  * @version: 1.0
  */
-class HomeFragment : BaseMvvmFragment<FragmentHomeBinding,HomeViewModel>(), OnRefreshListener {
+class HomeFragment : BaseMvvmFragment<FragmentHomeBinding, HomeViewModel>(), OnRefreshListener {
 
     private val mArrayTabFragments = SparseArray<Fragment>()
     private var mFragmentAdapter: ViewPage2FragmentAdapter? = null
@@ -50,7 +47,8 @@ class HomeFragment : BaseMvvmFragment<FragmentHomeBinding,HomeViewModel>(), OnRe
         mArrayTabFragments.append(0, HomeTabFragment1())
         mProjectTabs.add(0, ProjectTabItem(id = 0, "条目一"))
         activity?.let {
-            mFragmentAdapter = ViewPage2FragmentAdapter(childFragmentManager, lifecycle, mArrayTabFragments)
+            mFragmentAdapter =
+                ViewPage2FragmentAdapter(childFragmentManager, lifecycle, mArrayTabFragments)
 //            mFragmentAdapter = ViewPage2FragmentAdapter(it, mArrayTabFragments)
         }
         mBinding?.let {
@@ -61,9 +59,10 @@ class HomeFragment : BaseMvvmFragment<FragmentHomeBinding,HomeViewModel>(), OnRe
             //需要注意是FragmentStateAdapter不会一直保持Fragment实例，在被destroy后，需要做好Fragment重建后回复数据的准备，这点可以结合ViewModel来进行配合使用。
             it.viewPager.offscreenPageLimit = mArrayTabFragments.size()
 
-            mTabLayoutMediator = TabLayoutMediator(it.tabHome, it.viewPager) { tab: TabLayout.Tab, position: Int ->
-                tab.text = mProjectTabs[position].name
-            }
+            mTabLayoutMediator =
+                TabLayoutMediator(it.tabHome, it.viewPager) { tab: TabLayout.Tab, position: Int ->
+                    tab.text = mProjectTabs[position].name
+                }
             //tabLayout和viewPager2关联起来
             mTabLayoutMediator?.attach()
 
@@ -109,11 +108,11 @@ class HomeFragment : BaseMvvmFragment<FragmentHomeBinding,HomeViewModel>(), OnRe
     override fun initData() {
         super.initData()
 
-        mViewModel.getDataList().observe(this){
-
+        mViewModel.getDataList().observe(this) {
+            mBinding?.refreshLayout?.finishRefresh()
         }
 
-        mViewModel.getDataList2().observe(this){
+        mViewModel.getDataList2().observe(this) {
 
             mProjectTabs.add(ProjectTabItem(id = 1, "条目二"))
             mArrayTabFragments.append(1, HomeTabFragment2())
@@ -132,15 +131,14 @@ class HomeFragment : BaseMvvmFragment<FragmentHomeBinding,HomeViewModel>(), OnRe
         refresh()
     }
 
-    private fun refresh(){
+    private fun refresh() {
 //        mViewModel.launchUI(errorBlock = {code,err ->
 //
 //        }){
 //
 //        }
-        Handler(Looper.getMainLooper()).postDelayed(kotlinx.coroutines.Runnable {
-            mBinding?.refreshLayout?.finishRefresh()
-        },3000)
+
+        mBinding?.refreshLayout?.finishRefresh()
     }
 
     override fun onDestroy() {
