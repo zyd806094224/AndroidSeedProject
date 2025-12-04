57250769c639bb9d9c2f3e3875575084ac0683aa
zhaoyudong <zhaoyudong@staff.hexun.com>
2025-12-04 09:00:39 +0800

    升级agp到8.2.0、kotlin版本1.9.22

--- mod_main/src/main/java/com/demo/main/MainActivity.kt
+++ mod_main/src/main/java/com/demo/main/MainActivity.kt
@@ -15,7 +15,6 @@ import com.demo.common.service.RouterService
 import com.demo.framework.base.BaseDataBindActivity
 import com.demo.framework.utils.StatusBarSettingHelper
 import com.demo.main.databinding.ActivityMainBinding
-import com.demo.main.navigator.SelfFragmentNavigator
 
 /**
  * @Description: 主页
@@ -39,19 +38,34 @@ class MainActivity : BaseDataBindActivity<ActivityMainBinding>() {
     override fun initView(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         val navView = mBinding.navView
+
         //1.寻找出路由控制器对象，它是路由跳转的唯一入口，找到宿主NavHostFragment
-        navController = findNavController(R.id.nav_host_fragment_activity_main)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
-        //2.自定义FragmentNavigator，mobile_navigation.xml文件中的fragment标识改为SumFragmentNavigator的sumFragment
-        val fragmentNavigator =
-            SelfFragmentNavigator(this, navHostFragment.childFragmentManager, navHostFragment.id)
-        //3.注册到Navigator里面，这样才找得到
-        navController.navigatorProvider.addNavigator(fragmentNavigator)
-        //4.设置Graph，需要将activity_main.xml文件中的app:navGraph="@navigation/mobile_navigation"移除
-        navController.setGraph(R.navigation.mobile_navigation)
-        //5.将NavController和BottomNavigationView绑定，形成联动效果
-        navView.setupWithNavController(navController)
+
+        //2.获取NavController，使用标准Navigation组件
+        navController = navHostFragment.navController
+
+        //3.将NavController和BottomNavigationView绑定，形成联动效果
+        try {
+            navView.setupWithNavController(navController)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            // 备用导航方案
+            navView.setOnNavigationItemSelectedListener { item ->
+                when(item.itemId) {
+                    R.id.navi_home -> {
+                        navController.navigate(R.id.navi_home)
+                        true
+                    }
+                    R.id.navi_mine -> {
+                        navController.navigate(R.id.navi_mine)
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
         StatusBarSettingHelper.setStatusBarTranslucent(this)
         StatusBarSettingHelper.statusBarLightMode(this@MainActivity, true)
 
